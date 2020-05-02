62be282b2bcacbcb52dbfb68ec2db01ee9673fd2
albonicc <nicolasalbom@gmail.com>
2020-05-02 00:57:33 -0500

    Now LED status is saved into the Arduino's EEPROM, so in case power is lost, the LED remebers if it was on or off

--- ArduinoI2CSlave.ino
+++ ArduinoI2CSlave.ino
@@ -1,18 +1,25 @@
 /*
   Arduino Slave for Raspberry Pi Master
-  i2c_slave_ard.ino
   Connects to Raspberry Pi via I2C
-  
-  DroneBot Workshop 2019
-  https://dronebotworkshop.com
-*/
 
+  LED state is stored into the Arduino's
+  EEPROM, so when power is lost and then 
+  recovered, the LED will continue to be
+  on or off depending on how it was 
+  before.
+*/
+ 
 // Include the Wire library for I2C
 #include <Wire.h>
-
+#include <EEPROM.h>
+ 
 // LED on pin 13
-const int ledPin = 13; 
+const int ledPin = 13;
+char ledState;
 
+long lastDebounceTime = 0;
+long debounceDelay = 50; 
+ 
 void setup() {
   // Join I2C bus as slave with address 8
   Wire.begin(0x8);
@@ -22,16 +29,32 @@ void setup() {
   
   // Setup pin 13 as output and turn LED off
   pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, LOW);
+  checkLedState();
 }
-
+ 
 // Function that executes whenever data is received from master
 void receiveEvent(int howMany) {
   while (Wire.available()) { // loop through all but the last
-    char c = Wire.read(); // receive byte as a character
-    digitalWrite(ledPin, c);
+    ledState = Wire.read(); // receive byte as a character
+    digitalWrite(ledPin, ledState);
+    EEPROM.update(0, ledState);
   }
 }
 void loop() {
   delay(100);
+}
+
+// Prints and upates the LED state
+// when the Arduino board restarts or powers up
+void checkLedState() {
+   Serial.println("LED status after restart: ");
+   ledState = EEPROM.read(0);
+   if(ledState == 1) {
+    Serial.println ("ON");
+    digitalWrite(ledPin, HIGH);
+   } 
+   if(ledState == 0) {
+    Serial.println ("OFF");
+    digitalWrite(ledPin, LOW);
+   }
 }
