2b71bcfab9a0152ad949aeb21c7e301c4ea4b966
albonicc <nicolasalbom@gmail.com>
2020-05-04 00:34:57 -0500

    ArduinoEEPROMMAtrix.ino successfuly works!

--- ArduinoEEPROMMatrix.ino
+++ ArduinoEEPROMMatrix.ino
@@ -9,11 +9,11 @@
 #include <Wire.h>
 #include <EEPROM.h>
  
-int i = 0
-char c;
-
+byte dataArray[12];
  
 void setup() {
+  
+  Serial.begin(9600);
   // Join I2C bus as slave with address 8
   Wire.begin(0x8);
   
@@ -26,24 +26,54 @@ void setup() {
  
 // Function that executes whenever data is received from master
 void receiveEvent(int howMany) {
+  byte intermediate;
   while (Wire.available()) { // loop through all but the last
-    c = Wire.read(); // receive byte as a character
-    while(i < i + 19){
-        EEPROM.write(i, c);
-    } 
+    Serial.print("Datos mandados RPi");
+    Serial.print('\n');
+    for(int i = 0; i < howMany; i++){
+       intermediate = Wire.read();
+       if(intermediate == 0){
+          intermediate = 0;
+          continue;
+        }
+        else{
+          dataArray[i] = intermediate;
+          EEPROM.update(i, dataArray[i]);
+        }
+      Serial.print(dataArray[i]);
+      Serial.print(" ");
+    }
+    Serial.print('\n');
+//    Serial.print("Datos a guardar en la EEPROM");
+//    Serial.print('\n');
+//    for(int j = 0; j < 11; j++){
+//      Serial.print(dataArray[j]);
+//      Serial.print(" ");
+//      EEPROM.update(j, dataArray[j]);
+//      Serial.print(j);
+//      Serial.print(" ");
+//    } 
   }
 }
 void requestEvent(){
-    String message;
+    char message[12];
+    char message_original[13];
     char character;
-    while(i < 1+19){
+    Serial.print('\n');
+    Serial.print("Datos guardados en la EEPROM");
+    Serial.print('\n');
+    for(int i = 1; i < 13;i++){
         character = EEPROM.read(i);
-        message = String(message + character)
+        Serial.print(character);
+        message[i] = character;
     }
+    
+    
+    Serial.print('\n');
+    Serial.print(message);
     Wire.write(message);
 
 }
 void loop() {
   delay(100);
-}
 }
